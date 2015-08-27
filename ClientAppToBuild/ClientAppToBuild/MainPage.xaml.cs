f1ae18b639a2f32bbdbb8f0a6080747d14119b3d
Anthony Harrison <anthony.harrison@xamtastic.com>
2015-08-27 18:46:35 +0100

    Initial Commit

---
+++ ClientAppToBuild/ClientAppToBuild/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Com.Xamtastic.Patterns.SmallestMvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ClientAppToBuild
+{
+    [ViewModelType(typeof(MainPageViewModel))]
+    public partial class MainPage : PageBase
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
