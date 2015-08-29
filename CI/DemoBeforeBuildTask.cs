0dff030b1ae260f98c144fb2f78261db0dfb3d58
Anthony Harrison <anthony.harrison@xamtastic.com>
2015-08-29 16:17:23 +0100

    CI BeforeBuild and AfterBuild Tasks created for iOS

---
+++ CI/DemoBeforeBuildTask.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Build.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CI
+{
+    public class DemoBeforeBuildTask : Microsoft.Build.Utilities.Task
+    {
+        [Required]
+        public string ProjectName { get; set; }
+
+        [Required]
+        public string LogPath { get; set; }
+
+        public override bool Execute()
+        {
+
+            string path = string.Format(LogPath);
+            if (!File.Exists(path))
+            {
+                using (StreamWriter sw = File.CreateText(path))
+                {
+                    sw.WriteLine(string.Format("Build Server Log File Created: {0} UTC", DateTime.UtcNow.ToString()));
+                }
+            }
+
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(string.Format("Before Build Started for Project {0} at {1} UTC", ProjectName, DateTime.UtcNow.ToString()));
+            }
+
+            using (StreamWriter sw = File.AppendText(path))
+            {
+                sw.WriteLine(string.Format("Before Build Finished for Project {0} at {1} UTC", ProjectName, DateTime.UtcNow.ToString()));
+            }
+
+
+            return true;
+        }
+
+
+    }
+}
