2ba046e0ad59301c1d58e8205c350c1849716bc8
Anthony Harrison <anthony.harrison@xamtastic.com>
2015-08-30 17:03:59 +0100

    Restore forced fail build to normal

--- CI/DemoBeforeBuildTask.cs
+++ CI/DemoBeforeBuildTask.cs
@@ -153,8 +153,6 @@ namespace CI
 
         private bool InsertNextShortVersionBuildNumberInInfoPlistLineCollection()
         {
-            return false;
-
             if (_CFBundleShortVersionStringLineNumber != 0)
             {
                 _infoPlistLineCollection[_CFBundleShortVersionStringLineNumber + 1] = string.Format("<string>{0}</string>", _nextBuildNumber.Trim());
