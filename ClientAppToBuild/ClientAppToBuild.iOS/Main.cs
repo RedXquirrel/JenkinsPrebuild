7c178347ef6a8fb8fbe1c726e6fc6e6eae53bdb0
Anthony Harrison <anthony.harrison@xamtastic.com>
2015-08-31 18:26:29 +0100

    Force Build commit

--- ClientAppToBuild/ClientAppToBuild.iOS/Main.cs
+++ ClientAppToBuild/ClientAppToBuild.iOS/Main.cs
@@ -12,6 +12,9 @@ namespace ClientAppToBuild.iOS
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
+
+            int a = 1;
+            int b = a;
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
