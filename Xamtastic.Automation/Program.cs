766347b8c419d85225196a9058ad553781f736c3
Anthony Harrison <anthony.harrison@xamtastic.com>
2015-09-04 18:58:44 +0100

    Build commit

---
+++ Xamtastic.Automation/Program.cs
@@ -0,0 +1,24 @@
+﻿using Com.Xamtastic.Patterns.CI.Dropbox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Xamtastic.Automation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = Task.Run((Func<Task>)Program.Run);
+            task.Wait();
+        }
+
+        static async Task Run()
+        {
+            CIPostBuildTask postBuild = new CIPostBuildTask();
+            await postBuild.Run();
+        }
+    }
+}
